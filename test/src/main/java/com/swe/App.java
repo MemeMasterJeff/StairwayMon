f222c6387779dcaf43627eb97614dbdc26aa1a18
MemeMasterJeff <wxwang44@gmail.com>
2023-09-07 13:45:59 -0500

    works

--- test/src/main/java/com/swe/App.java
+++ test/src/main/java/com/swe/App.java
@@ -4,9 +4,7 @@ import com.fazecast.jSerialComm.SerialPort;
 /**
  * Hello world!
  */
-public final class App {
-    private App() {
-    }
+public class App {
 
     public static void main(String[] args) {
         SerialPort[] serialPorts = SerialPort.getCommPorts();
@@ -15,19 +13,18 @@ public final class App {
             System.err.println("No serial ports found.");
             return;
         }
-
-        SerialPort serialPort = serialPorts[0];//idk copilot did this lol
+        SerialPort serialPort = serialPorts[0];
         serialPort.setBaudRate(9600);
         serialPort.setNumDataBits(8);
         serialPort.setNumStopBits(1);
         serialPort.setParity(SerialPort.NO_PARITY);
-
+        serialPort.openPort();
         //reading part
         try {
            while (true) {
-                byte[] readbuffer = new byte[1024];
+                byte[] readbuffer = new byte[8];
                 int bytesRead = serialPort.readBytes(readbuffer, readbuffer.length);
-
+                System.out.println(bytesRead);
                 if (bytesRead > 0) {
                     System.out.println("Read " + bytesRead + " bytes.");
                     System.out.println("Data: " + new String(readbuffer, 0, bytesRead));
